43dc0ef9f3437c7235d20ddd72bd0e4f376c87db
Stanislav <novikovs@gmail.com>
2025-03-17 14:25:21 +0300

    wip orgs done

--- src/Requests/ResponseResolver.php
+++ src/Requests/ResponseResolver.php
@@ -411,7 +411,7 @@ class ResponseResolver
     protected function handleCreateDtoValidationException(CreateDtoValidationException $exception): void
     {
         if (app()->hasDebugModeEnabled()) {
-            $message = "The data received does not correspond to the declaration of {$exception->getClass()}. Please check the specified DTO class, also `handle` method of the client, resources or request";
+            $message = "The data received does not correspond to the declaration of {$exception->getClass()}. Please check field types and the specified DTO class, also `handle` method of the client, resources or request";
             $this->details = $exception->validator->errors()->all();
         } else {
             $message = "Data error, please contact the service administrator";
